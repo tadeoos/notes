24400b7fe7af8c8fd79dee94cb81b082abfc0148
Tadek Teleżyński <tadekte@gmail.com>
2017-03-25 00:38:02 +0100

    make notes dir hidden

--- notes.py
+++ notes.py
@@ -1,8 +1,11 @@
 #!/Users/Tadeo/anaconda/bin/python
+
 import os
 import sys
 
-file, note_text = 'notes/'+sys.argv[1], ' '.join(sys.argv[2:])+'\n'
-os.makedirs('notes', exist_ok=True)
+file, note_text = '.notes/'+sys.argv[1], ' '.join(sys.argv[2:])+'\n'
+
+os.makedirs('.notes', exist_ok=True)
+
 with open(file,"a+") as f:
     f.write(note_text)
