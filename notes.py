f7f9540e304b86c9d9621f25d0cf70ebec38ea79
Tadek Teleżyński <tadekte@gmail.com>
2017-03-25 00:35:26 +0100

    complete

---
+++ notes.py
@@ -0,0 +1,8 @@
+#!/Users/Tadeo/anaconda/bin/python
+import os
+import sys
+
+file, note_text = 'notes/'+sys.argv[1], ' '.join(sys.argv[2:])+'\n'
+os.makedirs('notes', exist_ok=True)
+with open(file,"a+") as f:
+    f.write(note_text)
