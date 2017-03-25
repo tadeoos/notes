b43451a7978d42094a90b3bcbba55398b426afb3
Tadek Teleżyński <tadekte@gmail.com>
2017-03-25 20:30:24 +0100

    add optparse module

--- notes.py
+++ notes.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 
 from datetime import datetime
+from optparse import OptionParser
 import os
 import sys
 
 def main():
-	if sys.argv[1] == '-i':
-		os.system('cp notes.py /usr/local/bin/notes.py')
-		return
 
-	args = sys.argv[1:]
+	usage = "usage: %prog [options] note_content"
+	description=' Simple project notes manager.'
+	parser = OptionParser(usage=usage, description=description)
+	parser.add_option("-i","--install", action="store_true", dest="install", help="copy script to the /usr/local/bin dir")
+	parser.add_option("-d","--date", action="store_true", dest="date", help="add date of creation at the end of the note_content")
+
+
+	options, args = parser.parse_args()
+
+	if options.install:
+		os.system('cp notes.py /usr/local/bin/notes.py')
+		return True
 
 	file = '.notes/'+args[0]
-	note_text = "- [ ] "+' '.join(args[1:])+'\t// added '+datetime.now().strftime('%d.%m.%Y at %H:%M')+'\n\n'
+	note_text = "- [ ] "+' '.join(args[1:])
+	if options.date:
+		note_text += '\t// added '+datetime.now().strftime('%d.%m.%Y at %H:%M')
+	note_text += '\n\n'
 
 	os.makedirs('.notes', exist_ok=True)
 
