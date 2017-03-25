11acdab87017aa97b0222ecc8fe33ff1a48d584f
Tadek Teleżyński <tadekte@gmail.com>
2017-03-25 01:06:48 +0100

    add install option, update readme

--- notes.py
+++ notes.py
@@ -1,11 +1,28 @@
 #!/Users/Tadeo/anaconda/bin/python
 
+from datetime import datetime
 import os
 import sys
 
-file, note_text = '.notes/'+sys.argv[1], ' '.join(sys.argv[2:])+'\n'
+def main():
+	if sys.argv[1] == '-i':
+		os.system('cp notes.py /usr/local/bin/notes.py')
+		args = sys.argv[2:]
+		return
+	args = sys.argv[1:]
 
-os.makedirs('.notes', exist_ok=True)
+	file = '.notes/'+args[0]
+	note_text = "- "+' '.join(args[1:])+'\t/ ('+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+')\n\n'
 
-with open(file,"a+") as f:
-    f.write(note_text)
+	# os.makedirs('.notes', exist_ok=True)
+	try:
+		os.mkdir('.notes')
+		with open(file,"w") as f:
+			f.write('# NOTES\n\n')
+	except FileExistsError:
+		pass
+	with open(file,"a+") as f:
+		f.write(note_text)
+
+if __name__ == '__main__':
+	main()
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # NOTES
 
 A simple script to create short notes while developing a project.
+
+## USAGE
+
+- first time run:
+`python notes.py -i`
+this will copy the script into /usr/local/bin directory. Assuming you have it in your path you can now run:
+`notes.py notes_file notes_content`
+from wherever on your computer.
+This will create (or append if it exist) a `notes_file` in .notes directory with 'notes_content' on a new line and the date of creation.
